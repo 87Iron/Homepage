96a05d2ed30c8ac4ed0600e40989fc0e6e399357
87Iron <kieranlrichardson@gmail.com>
2020-08-12 15:03:08 +0100

    CSS done. Enter Key needs work.

--- search.js
+++ search.js
@@ -1,13 +1,24 @@
 function search() {
   var input = document.getElementById('searchbar').value;
-  var spl = input.split(" ");
-  var webresult = "https://www.google.com/search?q=";
-  webresult = webresult + spl[0]
-  if(spl.length > 1){
-    for(i=1; i<spl.length; i++) {
-      webresult = webresult + "+"
-      webresult = webresult + spl[i]
+  var button = document.getElementById('searchbtn');
+  var searchbox = document.getElementById('searchbar');
+  
+  button.addEventListener("click", function(){
+    console.log(input);
+    var spl = input.split(" ");
+    var webresult = "https://www.google.com/search?q=";
+    webresult = webresult + spl[0]
+    if(spl.length > 1){
+      for(i=1; i<spl.length; i++) {
+        webresult = webresult + "+"
+        webresult = webresult + spl[i]
+      }
     }
-  }
-  window.location.href=webresult;
+    window.location.href=webresult;
+  });
+  searchbox.addEventListener("keyup", function(event){
+    if(event.keycode == 13) {
+      button.click();
+    }
+  });
 }
